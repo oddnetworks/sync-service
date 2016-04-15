78abbdbbc07abecbc94431abb886240629621569
Jose Gasca <jose@ooyala.com>
2016-04-15 10:45:57 -0500

    Remove itprotv require

--- agents/index.js
+++ agents/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 module.exports = {
 	brightcove: require('./brightcove'),
-	itprotv: require('./itprotv'),
 	itunes: require('./itunes'),
 	ooyala: require('./ooyala'),
 	rss: require('./rss')
